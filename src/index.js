c1f82b14d0271f35810f23afb2f4729a5bc8698e
Yuliia2991 <yuliiakurian@gmail.com>
2022-06-12 17:00:35 +0300

    Add a modal template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+<<<<<<< Updated upstream
+=======
+import './js/modal_footer';
+import './js/trending/trending';
+import './js/loader.js';
+import './js/tmdb';
+import './js/modal';
+>>>>>>> Stashed changes
