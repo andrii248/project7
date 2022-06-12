a72f3bbdbc02177d7e6db4b45dcef9f83df5c648
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-06-12 17:11:44 +0300

    add function fetch

--- src/js/watched_film_render.js
+++ src/js/watched_film_render.js
@@ -1,7 +1,20 @@
 // export default function addFilm
-
+BASE_URL = 'https://api.themoviedb.org/3/find';
+API_KEY = 'c1c0e09e2b2780ccf5e67712da2ef6c9',
+    
 localStorage.setItem("film:", { name: "Boys", genre: "action", id: "77777" });
 
 function renderWatchedFromLocalStorage() { 
-    console.log(localStorage.getItem("film"));
+    
 }
+
+function fetchSearchFilm() {
+    const searchUrl = `$BASE_URL/${filmId}?$API_KEY&language=en&external_source=imdb_id`
+
+    return fetch(url)
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error();
+        })
+    
+}    
