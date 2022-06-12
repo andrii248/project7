099deba7078275f652e6a04d9d22b64abb0db69f
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-06-12 11:56:57 +0300

    Merge branch 'ft-tmdb-store'

--- src/js/tmdb.js
+++ src/js/tmdb.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import storage from './storage';
 
 const API_KEY = 'c1c0e09e2b2780ccf5e67712da2ef6c9';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p';
 
 let genres = JSON.parse(
-  `[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]`,
+  `[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]`
 );
 
 const initialize = async () => {
@@ -125,36 +126,55 @@ const getMovie = async id => {
   };
 };
 
+const KEY_WATCHED = 'watched';
+const KEY_QUEUE = 'queue';
+const PAGE_LIMIT = 20;
+
+const store = {
+  watched: storage.load(KEY_WATCHED) || [],
+  queue: storage.load(KEY_QUEUE) || [],
+};
+
+const addToStore = (key, forSaving) => {
+  if (store[key].findIndex(item => item.id === forSaving.id) !== -1) {
+    return;
+  }
+  store[key].push(forSaving);
+  storage.save(key, store[key]);
+};
+
 const addToWatched = forSaving => {
-  console.log('In progress');
+  addToStore(KEY_WATCHED, forSaving);
 };
 
 const addToQueue = forSaving => {
-  console.log('In progress');
+  addToStore(KEY_QUEUE, forSaving);
+};
+
+const getFromStore = (key, page) => {
+  const array = store[key];
+  const totalPages = Math.ceil(array.length / PAGE_LIMIT);
+  const begin = (page - 1) * PAGE_LIMIT;
+  const end = begin + PAGE_LIMIT <= array.length ? begin + PAGE_LIMIT : array.length;
+  return {
+    page: page,
+    totalPages: totalPages,
+    movies: array.slice(begin, end),
+  };
 };
 
 const getWathed = page => {
-  console.log('In progress');
-  return {
-    page: page,
-    totalPages: 0,
-    movies: [],
-  };
+  return getFromStore(KEY_WATCHED, page);
 };
 
-const geQueue = page => {
-  console.log('In progress');
-  return {
-    page: page,
-    totalPages: 0,
-    movies: [],
-  };
+const getQueue = page => {
+  return getFromStore(KEY_QUEUE, page);
 };
 
-export { getTrending, searchMovie, getMovie };
+export { getTrending, searchMovie, getMovie, addToWatched, addToQueue, getWathed, getQueue };
 
 // EXAMPLES
-
+/*
 getTrending(10)
   .then(data => {
     console.log('Get trending', data);
@@ -170,5 +190,11 @@ searchMovie('movie')
 getMovie(705861)
   .then(data => {
     console.log('Get movie', data);
+    addToWatched(data.forSaving);
+    addToQueue(data.forSaving);
   })
   .catch(e => console.log(e));
+
+console.log('Get watched', getWathed(1));
+console.log('Get queue', getQueue(1));
+*/
