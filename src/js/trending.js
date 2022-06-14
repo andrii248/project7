260312789a9124161d34aad00c66def2c74fb79f
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-06-14 20:44:53 +0300

    pagination added

--- src/js/trending.js
+++ src/js/trending.js
@@ -1,14 +1,19 @@
 import { getTrending } from './tmdb';
 import filmCardsTpl from '../templates/films-cards.hbs';
+import initPagination from './pagination';
 
 const refs = {
   card: document.querySelector('.films__list'),
 };
 
-const trendingMarkup = page => {
-  getTrending(page).then(data => {
-    refs.card.innerHTML = filmCardsTpl(data.movies);
-  });
-};
+// const trendingMarkup = page => {
+//   getTrending(page).then(data => {
+//     refs.card.innerHTML = filmCardsTpl(data.movies);
+//   });
+// };
 
-trendingMarkup();
+// trendingMarkup();
+
+initPagination(getTrending, movies => {
+  refs.card.innerHTML = filmCardsTpl(movies);
+});
