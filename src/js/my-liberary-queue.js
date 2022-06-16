95bd5dd7375775450883a3c5a049f66f694b5598
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-06-16 18:59:31 +0300

    bugs fix

--- src/js/my-liberary-queue.js
+++ src/js/my-liberary-queue.js
@@ -1,19 +1,27 @@
 import initPagination from './pagination';
-import { getQueue } from './tmdb';
+import { getWatched } from './tmdb';
+import filmCardsTpl from '../templates/films-cards.js';
+import makeHeader from './heder-my-liberary';
 
-// const renderPageMyLibrary = movies => {
-//   console.log(movies);
-// };
+// makeHeader('library');
 
+const watchedBtn = document.querySelector(".watchedBtn");
+const myLibraryLink = document.querySelector(".nav__link-library");
+const homeLink = document.querySelector(".nav__link-home");
+console.log(watchedBtn);
 
-// function creeateBtnFunction() { 
-//   const queueBtn = document.querySelector("button .queueBtn")
-// //   console.log(queueBtn)
-// // } 
-// setTimeout(creeateBtnFunction, 1000);
+const refs = {
+  card: document.querySelector('.films__list'),
+};
 
-// const queueBtn = document.querySelector("button .queueBtn");
-// console.log(queueBtn)
+const renderPageMyLibrary = movies => {
+  const watchedMovies = getWatched(movies);
+  console.log(watchedMovies);
+  refs.card.innerHTML = filmCardsTpl(watchedMovies.movies);
+};
 
-// addEventListener("click", initPagination(getQueue, renderPageMyLibrary))
-//  
+watchedBtn.addEventListener("click", initPagination(getWatched, renderPageMyLibrary));
+myLibraryLink.addEventListener("click", () => { 
+  makeHeader('library');
+  initPagination(getWatched, renderPageMyLibrary);
+});
