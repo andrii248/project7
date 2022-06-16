6fab9c150de1eeb39a89e7f70a34d3ecfae7a79a
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-06-16 15:58:47 +0300

    wedding present

--- src/js/trending.js
+++ src/js/trending.js
@@ -12,30 +12,55 @@ const trendingMarkup = (page, range) => {
   getTrending(page, range).then(data => {
     refs.card.innerHTML = filmCardsTpl(data.movies);
   });
-  if (localStorage.getItem('theme') === 'dark') {setTimeout(() => {
-         const changeText = document.querySelectorAll('.films__title');         
-         for (let title of changeText) {
-             title.classList.add('dark');
+  if (localStorage.getItem('theme') === 'dark') {
+    setTimeout(() => {
+      const changeText = document.querySelectorAll('.films__title');
+      for (let title of changeText) {
+        title.classList.add('dark');
+      }
+    }, 500);
   }
-},500);}
 };
 
 //trendingMarkup();
 
-initPagination(getTrending, movies => {
+// initPagination(getTrending, movies => {
+//   refs.card.innerHTML = filmCardsTpl(movies);
+//   if (localStorage.getItem('theme') === 'dark') {
+//     setTimeout(() => {
+//       const changeText = document.querySelectorAll('.films__title');
+//       for (let title of changeText) {
+//         title.classList.add('dark');
+//       }
+//     }, 500);
+//   }
+// });
+
+const getTrendingDay = async page => {
+  return await getTrending(page, 'day');
+};
+
+const getTrendingWeek = async page => {
+  return await getTrending(page, 'week');
+};
+
+const renderPage = movies => {
   refs.card.innerHTML = filmCardsTpl(movies);
-   if (localStorage.getItem('theme') === 'dark') {setTimeout(() => {
-         const changeText = document.querySelectorAll('.films__title');         
-         for (let title of changeText) {
-             title.classList.add('dark');
+  if (localStorage.getItem('theme') === 'dark') {
+    setTimeout(() => {
+      const changeText = document.querySelectorAll('.films__title');
+      for (let title of changeText) {
+        title.classList.add('dark');
+      }
+    }, 500);
   }
-},500);}
-});
+};
 
 refs.week.addEventListener('click', onWeekButtonClick);
 
 function onWeekButtonClick() {
-  trendingMarkup(1, 'week');
+  // trendingMarkup(1, 'week');
+  initPagination(getTrendingWeek, renderPage);
   refs.day.toggleAttribute('disabled');
   refs.week.toggleAttribute('disabled');
 }
@@ -43,7 +68,10 @@ function onWeekButtonClick() {
 refs.day.addEventListener('click', onDayButtonClick);
 
 function onDayButtonClick() {
-  trendingMarkup(1, 'day');
+  // trendingMarkup(1, 'day');
+  initPagination(getTrendingDay, renderPage);
   refs.day.toggleAttribute('disabled');
   refs.week.toggleAttribute('disabled');
 }
+
+onDayButtonClick();
