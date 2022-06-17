686e74bce873094ba9f16dc9e64ba41aa1bd5082
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-06-17 23:20:44 +0300

    pagination mobile support

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -17,41 +17,42 @@ const setPage = async page => {
 
 const renderPagination = (page, totalPages) => {
   const buttons = [];
-
-  let min = page - 2;
-  if (min < 1) {
-    min = 1;
-  }
-  let max = page + 2;
-  if (max > totalPages) {
-    max = totalPages;
-  }
-  if (page > 1) {
-    buttons.push(`<a class="arrow arrow--left" data-page="${page - 1}"></a>`);
-  }
-  if (min > 1) {
-    buttons.push(`<a data-page="${1}">${1}</a>`);
-  }
-  if (min > 2) {
-    buttons.push(`<a class="dots"></a>`);
-  }
-  for (let i = min; i <= max; i += 1) {
-    if (i === page) {
-      buttons.push(`<a class="active" data-page="${i}">${i}</a>`);
-    } else {
-      buttons.push(`<a data-page="${i}">${i}</a>`);
+  if (totalPages > 1) {
+    const isMobile = window.matchMedia('(max-width: 600px)').matches;
+    let min = isMobile ? page - 1 : page - 2;
+    if (min < 1) {
+      min = 1;
+    }
+    let max = isMobile ? page + 1 : page + 2;
+    if (max > totalPages) {
+      max = totalPages;
+    }
+    if (page > 1 && !isMobile) {
+      buttons.push(`<a class="arrow arrow--left" data-page="${page - 1}"></a>`);
+    }
+    if (min > 1) {
+      buttons.push(`<a data-page="${1}">${1}</a>`);
+    }
+    if (min > 2) {
+      buttons.push(`<a class="dots"></a>`);
+    }
+    for (let i = min; i <= max; i += 1) {
+      if (i === page) {
+        buttons.push(`<a class="active" data-page="${i}">${i}</a>`);
+      } else {
+        buttons.push(`<a data-page="${i}">${i}</a>`);
+      }
+    }
+    if (max < totalPages - 1) {
+      buttons.push(`<a class="dots"></a>`);
+    }
+    if (max < totalPages) {
+      buttons.push(`<a data-page="${totalPages}">${totalPages}</a>`);
+    }
+    if (page < totalPages && !isMobile) {
+      buttons.push(`<a class="arrow arrow--right" data-page="${page + 1}"</a>`);
     }
   }
-  if (max < totalPages - 1) {
-    buttons.push(`<a class="dots"></a>`);
-  }
-  if (max < totalPages) {
-    buttons.push(`<a data-page="${totalPages}">${totalPages}</a>`);
-  }
-  if (page < totalPages) {
-    buttons.push(`<a class="arrow arrow--right" data-page="${page + 1}"</a>`);
-  }
-
   pagination.innerHTML = buttons.join('');
 };
 
