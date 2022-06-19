48d58875281e14927da9ac64a0afc982e8a8fe44
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-06-19 23:10:35 +0300

    fix render from watched and queue, add messaage when storageList is empty, fix height on poster of cards

--- src/js/my-liberary-render.js
+++ src/js/my-liberary-render.js
@@ -1,44 +1,51 @@
 import initPagination from './pagination';
 import { getQueue, getWatched } from './tmdb';
 import filmCardsTpl from '../templates/films-cards.js';
-import addDark from './dark_theme';
-import { filmTitleDark } from './dark_theme';
+// import addDark from './dark_theme';
+// import { filmTitleDark } from './dark_theme';
 
 const refs = {
-  card: document.querySelector('.films__list'),
+  cardsList: document.querySelector('.films__list'),
   container: document.querySelector('header .container'),
+  theme: document.querySelector('theme'),
 };
 
-// const renderPage = movies => refs.card.innerHTML = filmCardsTpl(movies);
+const renderPage = movies =>
+  movies.length
+  ? refs.cardsList.innerHTML = filmCardsTpl(movies)
+  : refs.cardsList.innerHTML = createMessageEmptyList();
 
-// refs.container.addEventListener("click", (event) => {
-//   event.preventDefault();
+refs.container.addEventListener("click", (event) => {
+  // event.preventDefault();
 
-//   const elementClassList = event.target.classList;
-//   const watchedBtn = refs.container.lastChild.firstChild;
-//   const queueBtn = refs.container.lastChild.lastChild;
-//   const totalPagesWatched = Number(getWatched().totalPages);
-//   const totalPagesQueue = Number(getQueue().totalPages);
-//   const pagination = document.querySelector('.pagination');
+  const elementClassList = event.target.classList;
+  const watchedBtn = refs.container.lastChild.firstChild;
+  const queueBtn = refs.container.lastChild.lastChild;
+  const totalPagesWatched = Number(getWatched().totalPages);
+  const totalPagesQueue = Number(getQueue().totalPages);
+  const pagination = document.querySelector('.pagination');
 
-//   if (elementClassList.contains("watchedBtn")) {
-//     initPagination(getWatched, renderPage);
+  if (elementClassList.contains("watchedBtn")) {
+    initPagination(getWatched, renderPage);
+    watchedBtn.classList.add("activeBtn");
+    queueBtn.classList.remove("activeBtn");
+    if (totalPagesWatched === 1) {
+      pagination.classList.add("visually-hidden");
+    } else { pagination.classList.remove("visually-hidden") };
 
-//     watchedBtn.classList.add("activeBtn");
-//     queueBtn.classList.remove("activeBtn");
-//     if (totalPagesWatched === 1) {
-//       pagination.classList.add("visually-hidden");
-//     } else { pagination.classList.remove("visually-hidden") };
+  }
 
-//   }
+  if (elementClassList.contains("queueBtn")) {
+    initPagination(getQueue, renderPage);
+    console.log(getWatched().movies.length);
+    queueBtn.classList.add("activeBtn");
+    watchedBtn.classList.remove("activeBtn");
+    if (totalPagesQueue === 1) {
+      pagination.classList.add("visually-hidden");
+    } else { pagination.classList.remove("visually-hidden") };
+  }
+})
 
-//   if (elementClassList.contains("queueBtn")) {
-//     initPagination(getQueue, renderPage);
-//     queueBtn.classList.add("activeBtn");
-//     watchedBtn.classList.remove("activeBtn");
-//     if (totalPagesQueue === 1) {
-//       pagination.classList.add("visually-hidden");
-//     } else { pagination.classList.remove("visually-hidden") };
-//   }
-// })
-
+const createMessageEmptyList = () => {
+  return '<div class="emptyLibrare"><p class="emptyLibrare__text"> your movie list is empty. add a movie to your library</p></div>';
+}
--- src/templates/films-cards.js
+++ src/templates/films-cards.js
@@ -6,13 +6,15 @@ export default function filmCardsTpl(movies, votesHidden = true) {
   return movies
     .map(
       ({ image, title, genres, year, vote, id }) => `<li class="films__item ${darkThemeCardClass}">
-    <a class="films__link" href="#">
-      <picture>
-            <source srcset=${image.desktop} media="(min-width: 1024px)">
-            <source srcset=${image.tablet} media="(min-width: 768px)">
-            <source srcset=${image.mobile} media="(max-width: 767px)">
-            <img class="films__img" src=${image.desktop} width = '100%' alt=${title}" data-id='${id}' loading="lazy">
-      </picture>
+    <a class="films__link" href="#">\
+      <div class="picture__wrap">
+        <picture>
+              <source srcset=${image.desktop} media="(min-width: 1024px)">
+              <source srcset=${image.tablet} media="(min-width: 768px)">
+              <source srcset=${image.mobile} media="(max-width: 767px)">
+              <img class="films__img" src=${image.desktop} width = '100%' alt=${title}" data-id='${id}' loading="lazy">
+        </picture>
+      </div>
       <h2 class="films__title ${darkThemeTitleClass}">${title}</h2>
       <p class="films__info">
         ${genres} | ${year} <span class="films__vote ${votesVisibleValue}">${vote}</span>
