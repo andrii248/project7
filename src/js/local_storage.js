70235bb3db0617200cc8a88f6a30ec6912a05cc7
GudymaVit <gudymavit1988@gmail.com>
2022-06-20 00:19:46 +0300

    update modal button and localstorege

--- src/js/local_storage.js
+++ src/js/local_storage.js
@@ -1,4 +1,4 @@
-import { addToWatched, addToQueue, getMovie } from '../js/tmdb';
+import { addToWatched, addToQueue, getMovie,findInWatched,removeFromWatched,findInQueue,removeFromQueue } from '../js/tmdb';
 
 const refs = {
     modalContainer: document.querySelector('.modal__container'),
@@ -7,17 +7,33 @@ const refs = {
 refs.modalContainer.addEventListener('click', btnClick);
 
 function btnClick(e) {
-    let btnName = e.target.className;
-    let filmId = e.target.parentElement.dataset.id;
-
+    const btnName = e.target.className;
+    const filmId = Number(e.target.parentElement.dataset.id);
+    // if (btnName !== 'modal__btn modal__btn--watched') { return };
     if (btnName === 'modal__btn modal__btn--watched') {
-        getMovie(filmId)
-            .then(data => { addToWatched(data.forSaving) })
-            .catch(e => console.log(e));
+        if (findInWatched(filmId)) {
+            removeFromWatched(filmId);
+            e.target.textContent = 'Add to watched';
+        }
+        else {
+            getMovie(filmId)
+            .then(data => { addToWatched(data.forSaving)})
+                .catch(e => console.log(e));
+            e.target.textContent = 'Remove from watched';
+            
+        }
     }
     if (btnName === 'modal__btn modal__btn--queue') {
-        getMovie(filmId)
+        if (findInQueue(filmId)) {
+            removeFromQueue(filmId);
+            e.target.textContent = 'Add to queue';
+        }
+        else {
+            getMovie(filmId)
             .then(data => { addToQueue(data.forSaving) })
-            .catch(e => console.log(e));
+                .catch(e => console.log(e));
+            e.target.textContent = 'Remove from queue';
+        }
     }
+    
 }
