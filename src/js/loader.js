ac5ab485465570908e3b9792494d74fa1ee74bb3
Pavel <parbeck17@gmail.com>
2022-06-19 16:22:03 +0300

    Fix footer modal and move spiner

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,13 +1,26 @@
 class Spiner {
    constructor(linkOnSpiner){
+      this.spinerReady = false;
       this.spiner = document.querySelector(linkOnSpiner);
    }
 
    spinerStart(){
+      this.spinerReady = false;
+      setTimeout(()=>{
+         this.spinerReady = true;
+      }, 300);
       this.spiner.classList.add("spiner-to-go-js");
-      console.log(2);
    }
    spinerEnd(){
-      this.spiner.classList.remove("spiner-to-go-js");
+      if(this.spinerReady){
+         this.spiner.classList.remove("spiner-to-go-js");
+      }
+      else {
+         setTimeout(()=>{
+            this.spiner.classList.remove("spiner-to-go-js");
+         }, 300);
+      }
    }
 }
+const spiner = new Spiner("[data-spiner]");
+export { spiner };
